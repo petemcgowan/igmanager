aeaa0cd397d372e30e3999793aa6920ddcc8cf16
Pete <peteburquette@gmail.com>
2021-07-07 16:33:56 +0100

    example config files

---
+++ config/igconfig example.js
@@ -0,0 +1,9 @@
+export const igconfig = {
+  // your IG user id (this one is random)
+  instagramId: "9998887777",
+  // the user agent your IG is using
+  userAgent:
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
+  // the cookie retrieved from your IG login session
+  cookieString: "big long cookie string featuring many elements",
+};
---
+++ config/igconfig_variables example.js
@@ -0,0 +1,8 @@
+export const igconfig_variables = {
+  // your IG user id
+  instagramId: 9998887777,
+  // typically your IG user id
+  followedById: 9998887777,
+  // typically your IG user id
+  followingId: 9998887777,
+};
